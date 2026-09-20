03f241772623b473adc1ecc04e9a252e2c75b030
Diganta <digantasamanta90@gmail.com>
2026-09-20 23:27:57 +0530

    q 14

--- Day 10/script.js
+++ Day 10/script.js
@@ -1,50 +1,32 @@
-
-// Q7
-
-// 7
-
-// let age = prompt("Enter your age");
-
-// if (age === null || age.trim() === "") {
-//     console.log("Invalid input");
-// } else if (isNaN(age) || Number(age) < 0) {
-//     console.log("Invalid input");
-// } else if (Number(age) >= 18 && Number(age) <110) {
-//     console.log("Eligible");
-// }else if (Number(age) >= 110) {
-//     console.log("Are you fucking mad?");
+//7 Check if user is eligible to vote
+// let age = Number(prompt("Enter your age:"));
+// if (age >= 18) {
+// console.log("Eligible");
 // } else {
-//     console.log("Not eligible");
+// console.log("Not eligible");
 // }
 
 
-// 8
-
+//8 Print multiplication table of 5
 // for (let i = 1; i <= 10; i++) {
-//     console.log(`5 x ${i} = ${5 * i}`);
+// console.log("5 x " + i + " = " + (5 * i));
 // }
 
 
-// 9
-
+//9 Count numbers greater than 8 between 1 and 15
 // let count = 0;
-
 // for (let i = 1; i <= 15; i++) {
-//     if (i > 8) {
-//         count++;
-//     }
+// if (i > 8) {
+// count++;
 // }
+// }
+// console.log("Count = " + count);
 
-// console.log(count);
 
-// Q10
-// 10
-
-// 10
+//10 Check password and print access status
 
 // let password = prompt("Enter password");
 // let againPassword = prompt("Enter password again");
-
 // let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).+$/;
 // if (passwordRegex.test(password) && password === againPassword) {
 //     console.log("Access granted");
@@ -53,8 +35,7 @@
 // }
 
 
-//11.
-
+//11 Allow only 3 password attempts
 // let correctPassword = "12345";
 // let success = false;
 // for (let i = 1; i <= 3; i++) {
@@ -72,8 +53,7 @@
 // }
 
 
-// 12
-
+//12 Count how many times user types yes
 // let count = 0;
 // while (true) {
 // let word = prompt("Enter a word:");
@@ -87,9 +67,7 @@
 // console.log("yes typed = " + count);
 
 
-//13.
-
-//javascript
+//13 Print numbers divisible by 7 from 1 to 50
 // for (let i = 1; i <= 50; i++) {
 // if (i % 7 === 0) {
 // console.log(i);
@@ -97,9 +75,7 @@
 // }
 
 
-//14.
-
-//javascript
+//14 Find sum of all odd numbers from 1 to 30
 // let sum = 0;
 // for (let i = 1; i <= 30; i++) {
 // if (i % 2 !== 0) {
@@ -107,4 +83,3 @@
 // }
 // }
 // console.log("Sum = " + sum);
-
