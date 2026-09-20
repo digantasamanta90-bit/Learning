e427bb9e7692e409fd7dc1f7bd15cbcb3a0bf382
Diganta <digantasamanta90@gmail.com>
2026-09-20 23:37:50 +0530

    q 20

--- Day 10/script.js
+++ Day 10/script.js
@@ -83,3 +83,80 @@
 // }
 // }
 // console.log("Sum = " + sum);
+
+//15 Keep asking until user enters an even number
+// let num;
+// while (true) {
+// num = Number(prompt("Enter a number:"));
+// if (num % 2 === 0) {
+// break;
+// }
+// }
+// console.log("You entered an even number");
+
+
+//16 Print numbers between two user inputs
+// let start = Number(prompt("Enter start number:"));
+// let end = Number(prompt("Enter end number:"));
+// for (let i = start; i <= end; i++) {
+// console.log(i);
+// }
+
+
+//17 Print first 3 odd numbers from 1 to 20
+// let count = 0;
+// for (let i = 1; i <= 20; i++) {
+// if (i % 2 !== 0) {
+// console.log(i);
+// count++;
+// }
+// if (count === 3) {
+// break;
+// }
+// }
+
+
+//18 Count positive numbers among 5 inputs
+// let positive = 0;
+// for (let i = 1; i <= 5; i++) {
+// let num = Number(prompt("Enter number " + i + ":"));
+// if (num > 0) {
+// positive++;
+// }
+// }
+// console.log("Positive numbers = " + positive);
+
+
+//19 ATM simulator with 3 withdrawals
+// let balance = 1000;
+// for (let i = 1; i <= 3; i++) {
+// let amount = Number(prompt("Enter withdrawal amount:"));
+// if (amount <= balance) {
+// balance = balance - amount;
+// console.log("Withdrawal successful");
+// console.log("Balance = " + balance);
+// } else {
+// console.log("Insufficient balance");
+// }
+// }
+
+
+//20 Guess a random number between 1 and 20 in 5 attempts
+// let target = Math.floor(Math.random() * 20) + 1;
+// let guessed = false;
+// for (let i = 1; i <= 5; i++) {
+// let guess = Number(prompt("Guess a number between 1 and 20:"));
+// if (guess === target) {
+// console.log("Correct guess");
+// guessed = true;
+// break;
+// } else if (guess < target) {
+// console.log("Too low");
+// } else {
+// console.log("Too high");
+// }
+// }
+// if (guessed === false) {
+// console.log("You lost. Number was " + target);
+// }
+
